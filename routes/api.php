78de5066600c6ca8aa52bdac8712b1c7b1494f5c
yusdi17 <yusdipathudin@gmail.com>
2026-01-05 07:28:38 +0700

    get product

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
-use App\Http\Controllers\AddTransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\PendapatanController;
 use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\AddTransactionController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -16,3 +17,4 @@ Route::post('/login', [LoginController::class, 'login']);
 Route::get('/transactions', [TransactionController::class, 'index']);
 Route::get('/fee-today', [PendapatanController::class, 'getTodayIncome']);
 Route::post('/transactions', [AddTransactionController::class, 'store']);
+Route::get('/products', [ProductController::class, 'index']);
