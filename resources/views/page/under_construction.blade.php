169cd47fde7a3c01e1ce3647130fdd9943ecc640
yusdi17 <yusdipathudin@gmail.com>
2026-03-01 17:15:21 +0700

    fix error

--- resources/views/page/under_construction.blade.php
+++ resources/views/page/under_construction.blade.php
@@ -391,7 +391,7 @@
                     Download sekarang dan mulai atur keuanganmu!
                 </p>
 
-                <a href="{{ asset('apk/cuantrack_beta_version.apk') }}" download="CuanTrack.apk"
+                <a href="{{ asset('downloads/cuantrack_beta_version.apk') }}" download="CuanTrack.apk"
                    class="bg-white text-cuan-600 font-bold py-4 px-8 rounded-full shadow-lg hover:shadow-xl opacity-90 hover:opacity-100 transition-all flex w-fit items-center justify-center gap-3 mx-auto">
                     <svg class="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                         <path d="M325.3 234.3L104.6 13l280.8 161.2-60.1 60.1zM47 0C34 6.8 25.3 19.2 25.3 35.3v441.3c0 16.1 8.7 28.5 21.7 35.3l256.6-256L47 0zm425.2 225.6l-58.9-34.1-65.7 64.5 65.7 64.5 60.1-34.1c18-14.3 18-46.5-1.2-60.8zM104.6 499l280.8-161.2-60.1-60.1L104.6 499z" />
