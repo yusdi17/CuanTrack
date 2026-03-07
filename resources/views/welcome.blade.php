3a586f4c735e970fa5ab6846acb2f5bd673d575c
yusdi17 <yusdipathudin@gmail.com>
2026-03-07 17:04:53 +0700

    update done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
+        <link rel="icon" type="image/png" href="{{ asset('assets/logo.png') }}">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         <!-- Styles / Scripts -->
