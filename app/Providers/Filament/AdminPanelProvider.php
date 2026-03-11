8dd2de1654928a52af4eb256c087c64e38fe2e4e
yusdi17 <yusdipathudin@gmail.com>
2026-03-11 20:27:52 +0700

    update

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -35,6 +35,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->favicon(asset('assets/logo.png'))
             ->brandLogo(asset('assets/logo.png'))
+            ->brandLogoHeight('3rem')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
