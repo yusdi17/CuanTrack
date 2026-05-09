3168fc11bf208ebcba413c2732db3b786c0f2fbe
yusdi17 <yusdipathudin@gmail.com>
2026-05-09 17:48:23 +0700

    up

--- resources/views/page/under_construction.blade.php
+++ resources/views/page/under_construction.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CuanTrack - Segera Hadir</title>
+    <title>CuanTrack Download</title>
 
     <link rel="icon" type="image/png" href="{{ asset('assets/logo.png') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
